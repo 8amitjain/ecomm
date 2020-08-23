7877c57b819065bc9cdb76c3d4f94386da613d32
amitj-coder <amitj-coder@users.noreply.github.com>
2020-08-23 22:52:18 +0530

    Bug fixes

--- dawaiwala/data.py
+++
@@ -1,8 +0,0 @@
-
-Secret_key = 'O6mqd])@B@TyBqYLVjVMbDclpCIubUyB7AtXcBQJuVLibBu5Qo2G{JfN+yN{{N'
-
-Email = 'visionboard.help@gmail.com'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_51GziiaEMM8ICDJpWkzDBSC4IRrZZiae23SdzYedj0uPaKdSYwf6jJui0VfF2RFgMa1looBaTq82qRrvsLe0wkFqm00llm82WLu'
-STRIPE_SECRET_KEY = 'sk_test_51GziiaEMM8ICDJpWgnWlUH3cBzh6IOrYqTPpB7SgD5ZCZWb4sHcoa6WWd5BU69IhngDN0igApImzgA9aiZZj9k0y006rsHOJiB'
-
-GOOGLE_MAP_API_KEY = "AIzaSyBwRaz6Qcmyquj2GG5g4RChfBecOg641Qg"
